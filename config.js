81a4ad5584a050dac995ae0269fd936068078165
NeoNarwhal <142155258+vzepify@users.noreply.github.com>
2026-09-15 02:15:40 -0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-/*
-  GitHub Pages configuration.
-  Replace this URL with the public HTTPS URL of your Nexus IPTV API.
-  Example:
-  window.NEXUS_PORTAL_API = "https://api.example.com";
-*/
-window.NEXUS_PORTAL_API = "https://api.neonarwhal.qzz.io";
